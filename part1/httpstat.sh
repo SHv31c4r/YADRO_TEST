d31bca427b548280420c3021b43dc59714e4b97e
Shv31c4r <shv31c4r@users.noreply.github.com>
2026-05-10 17:05:00 +0300

    Added randomize for CODELIST

--- part1/httpstat.sh
+++ part1/httpstat.sh
@@ -1,16 +1,34 @@
 #! /bin/bash
-touch tmpBody
-CODELIST=(200 201 301 404 500)
+TMPFILE=$(mktemp)
+
+set -u
+
+if [ "$1" == "auto" ] 
+# Выполнить 5 запросов (по 1 на класс)
+then
+	CODES2XX=(200 201 202 203 204 205 206 207 208 226)
+	RAND2XX=${CODES2XX[$RANDOM % ${#CODES2XX[@]}]}
+	CODES4XX=(400 401 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 421 422 423 424 426 428 431 451) 
+	RAND4XX=${CODES4XX[$RANDOM % ${#CODES4XX[@]}]}
+	CODES5XX=(500 501 502 503 504 505 506 507 508 510 511)
+	RAND5XX=${CODES5XX[$RANDOM % ${#CODES5XX[@]}]}
+	CODELIST=($((100+$RANDOM % 4)) $RAND2XX $((300+$RANDOM % 9)) $RAND4XX $RAND5XX)
+# Выполнить запросы к указанным кодам
+else
+	read -p "Введите коды: " -a  CODELIST
+fi
+
+LINK="https://httpbin.org/status"
 
 for CODE in "${CODELIST[@]}" 
 do
-	STATUS=$(curl -o tmpBody -s -w "%{http_code}"  https://httpstat.us/$CODE)
-	echo "Trying URL: https://httpstat.us/$CODE"
+	echo "Trying URL: $LINK/$CODE"
+	STATUS=$(curl -o $TMPFILE -s -w "%{http_code}"  $LINK/$CODE)
 	if [ "$STATUS" -ge 400 ] 
 	then
-		echo "Exception: recieved status $STATUS" >&2
+		echo "EXCEPTION: recieved status $STATUS ($(cat $TMPFILE))" >&2
 	else
-		echo "$STATUS: $(cat tmpBody)	$(date +%y.%m.%d-%H:%M:%S)"
+		echo "$STATUS: $(cat $TMPFILE)	$(date +%d.%m.%y-%H:%M:%S)"
 	fi
 done
-rm tmpBody
+rm "$TMPFILE"
