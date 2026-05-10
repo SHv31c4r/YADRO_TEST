992648fbfad3e5b672b3f8cafadea4157fbe5625
Shv31c4r <shv31c4r@users.noreply.github.com>
2026-05-10 19:31:23 +0300

    Updated script

--- part1/httpstat.sh
+++ part1/httpstat.sh
@@ -1,30 +1,30 @@
 #! /bin/bash
 TMPFILE=$(mktemp)
+LINK="https://httpstat.us"
 
-set -u
+unset HTTP_PROXY http_proxy HTTPS_PROXY https_proxy
 
 if [ "$1" == "auto" ] 
-# Выполнить 5 запросов (по 1 на класс)
-then
-	CODES2XX=(200 201 202 203 204 205 206 207 208 226)
-	RAND2XX=${CODES2XX[$RANDOM % ${#CODES2XX[@]}]}
-	CODES4XX=(400 401 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 421 422 423 424 426 428 431 451) 
-	RAND4XX=${CODES4XX[$RANDOM % ${#CODES4XX[@]}]}
-	CODES5XX=(500 501 502 503 504 505 506 507 508 510 511)
-	RAND5XX=${CODES5XX[$RANDOM % ${#CODES5XX[@]}]}
-	CODELIST=($((100+$RANDOM % 4)) $RAND2XX $((300+$RANDOM % 9)) $RAND4XX $RAND5XX)
-# Выполнить запросы к указанным кодам
-else
-	read -p "Введите коды: " -a  CODELIST
+then # Выполнить 5 запросов к разным случайным кодам
+	ALLCODES=( $(curl -s -A "Mozilla/5.0" $LINK/ | grep -E -o '[1-5][0-9][0-9]' | sort -u) )
+	echo "Codes in list: ${#ALLCODES[@]}"
+	if [ ${#ALLCODES[@]} -eq 0 ] 
+	then
+		echo "Swap to the planB"
+		ALLCODES=(100 101 102 103 200 201 202 300 301 400 401 402 404 500 501 502)
+	fi
+	CODELIST=( $(shuf -n 5 -e "${ALLCODES[@]}") )
+else # Выполнить запросы к указанным кодам
+	read -p "Enter codes: " -a  CODELIST
 fi
 
-LINK="https://httpbin.org/status"
-
+# А теперь опрашиваем
 for CODE in "${CODELIST[@]}" 
 do
-	echo "Trying URL: $LINK/$CODE"
-	STATUS=$(curl -o $TMPFILE -s -w "%{http_code}"  $LINK/$CODE)
-	if [ "$STATUS" -ge 400 ] 
+	FULLPATH="$LINK/$CODE"
+	echo "Trying URL: $FULLPATH"
+	STATUS=$(curl -A "Mozilla/5.0" -o $TMPFILE -s -w "%{http_code}"  $FULLPATH)
+	if [[ "$STATUS" -ge 400 || "$STATUS" -eq 0 ]] 
 	then
 		echo "EXCEPTION: recieved status $STATUS ($(cat $TMPFILE))" >&2
 	else
